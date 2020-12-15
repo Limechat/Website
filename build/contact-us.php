1197a8e3d63d6b07a9803bcaec95a9c9992c79a9
vishal-pandey <contact@vishalpandey.co.in>
2020-12-15 19:08:15 +0530

    Responsive changes

--- build/contact-us.php
+++ build/contact-us.php
@@ -113,7 +113,7 @@
                </div>
                <div class="col-lg-3 text-right">
                   <ul class="login">
-                     <li class="d-inline active"><a href="contact-us.html">Get Demo</a></li>
+                     <li class="d-inline active"><a href="contact-us.php">Get Demo</a></li>
                   </ul>
                </div>
             </div>
@@ -121,6 +121,16 @@
       </header>
       <!--=================================
          Header -->
+
+
+
+         <style type="text/css">
+            @media(max-width:600px) {
+               .navbar-brand img{
+                  width: 150px;
+               }
+            }
+         </style>
       <!--=================================
          Banner -->
       <div class="breadcrumbs">
@@ -152,7 +162,7 @@
                   <div class="col-lg-6">
                      <div class="contact-box">
                         <h3>Get in Touch</h3>
-                        <form class="p-0" id="contact" method="POST" action="send.php">
+                        <form class="p-0" id="contact" method="POST">
                            <div class="contact-form mt-4">
                               <div class="row">
                                  <div class="col-lg-6 col-sm-12 mb-3">
@@ -247,6 +257,187 @@
       <!--=================================
          MAIN CONTENT -->
             
+<div style="display: none;">
+<?php
+   use PHPMailer\PHPMailer\PHPMailer;
+   use PHPMailer\PHPMailer\SMTP;
+   use PHPMailer\PHPMailer\Exception;
+   // Import PHPMailer classes into the global namespace
+   // These must be at the top of your script, not inside a function
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+   // Load Composer's autoloader
+   require 'vendor/autoload.php';
+
+   // Instantiation and passing `true` enables exceptions
+   $mail = new PHPMailer(true);
+
+
+
+
+
+   // Building message
+
+
+
+   $MESSAGE_BODY='<table style=" background:#F4F4F4 ; text-align : center">
+           
+           <tr>
+                   <th colspan="2" style="padding:10px;">
+                           <b>Contact Details</b>
+                   </th>
+           </tr>
+           <tr>
+                   <td style="padding:10px;">
+                           <b>First Name:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["f_name"] . '</td>
+           </tr>
+
+           <tr>
+                   <td style="padding:10px;">
+                           <b>Last Name:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["l_name"] . '</td>
+           </tr>
+
+           
+           <tr>
+                   <td style="padding:10px;">
+                           <b>E-mail:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["email"] . '</td>
+           </tr>
+           <tr>
+                   <td style="padding:10px;">
+                           <b>Contact:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["phone"] . '</td>
+           </tr>
+
+
+           <tr>
+                   <td style="padding:10px;">
+                           <b>Country:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["country"] . '</td>
+           </tr>
+           <tr>
+                   <td style="padding:10px;">
+                           <b>Company:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["company"] . '</td>
+           </tr>
+
+           <tr>
+                   <td style="padding:10px;">
+                           <b>Designation:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["designation"] . '</td>
+           </tr>
+           
+           <tr>
+                   <td style="padding:10px;">
+                           <b>Message:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["message"] . '</td>
+           </tr>
+   </table>';
+
+
+   // End building message
+
+
+
+
+
+   try {
+       //Server settings
+       $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+       $mail->isSMTP();                                            // Send using SMTP
+       $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+       $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+       $mail->Username   = getenv("USERNAME");                     // SMTP username
+       $mail->Password   = getenv("Password");                               // SMTP password
+       $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+       $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+       //Recipients
+       $mail->setFrom('admin@limechat.ai', 'Limechat AI');
+       $mail->addAddress('admin@limechat.ai', 'Limechat AI');     // Add a recipient
+       $mail->addReplyTo('admin@limechat.ai', 'Limechat AI');
+
+
+       // Content
+       $mail->isHTML(true);                                  // Set email format to HTML
+       $mail->Subject = 'New Get Demo Form Submit';
+       $mail->Body    = $MESSAGE_BODY;
+       $mail->AltBody = 'HTML MESSAGE IS NOT SUPPORT PLEASE CHECK';
+
+       $mail->send();
+       echo 'Message has been sent';
+   } catch (Exception $e) {
+       echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+   }
+
+   echo '<script type="text/javascript"> window.onload = ()=>{ $(".thank-button").click(); } </script>';
+
+
+}
+?>
+   
+</div>
+
+
+
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary thank-button" data-toggle="modal" data-target="#exampleModal" style="display: none;">
+  Launch demo modal
+</button>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Thank you</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Thank for contacting is we will reach out to you soon.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>   
+
+
+<div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
             <!--=================================
          Footer -->
       <footer>
@@ -269,7 +460,7 @@
                         <li class="d-inline ml-4 mr-4"><a href="sales.html">Sales</a></li><br>
                         <li class="d-inline ml-4 mr-4"><a href="support.html">Customer Support</a></li><br>
                         <li class="d-inline ml-4 mr-4"><a href="faq.html">FAQs</a></li><br>
-                        <li class="d-inline ml-4 mr-4"><a href="contact-us.html">Contact Us</a></li><br>
+                        <li class="d-inline ml-4 mr-4"><a href="contact-us.php">Contact Us</a></li><br>
                      </ul>
                   </div>
 
--- contact-us.php
+++ contact-us.php
@@ -32,7 +32,7 @@ layout: page
                   <div class="col-lg-6">
                      <div class="contact-box">
                         <h3>Get in Touch</h3>
-                        <form class="p-0" id="contact" method="POST" action="send.php">
+                        <form class="p-0" id="contact" method="POST">
                            <div class="contact-form mt-4">
                               <div class="row">
                                  <div class="col-lg-6 col-sm-12 mb-3">
@@ -127,3 +127,184 @@ layout: page
       <!--=================================
          MAIN CONTENT -->
             
+<div style="display: none;">
+<?php
+   use PHPMailer\PHPMailer\PHPMailer;
+   use PHPMailer\PHPMailer\SMTP;
+   use PHPMailer\PHPMailer\Exception;
+   // Import PHPMailer classes into the global namespace
+   // These must be at the top of your script, not inside a function
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+   // Load Composer's autoloader
+   require 'vendor/autoload.php';
+
+   // Instantiation and passing `true` enables exceptions
+   $mail = new PHPMailer(true);
+
+
+
+
+
+   // Building message
+
+
+
+   $MESSAGE_BODY='<table style=" background:#F4F4F4 ; text-align : center">
+           
+           <tr>
+                   <th colspan="2" style="padding:10px;">
+                           <b>Contact Details</b>
+                   </th>
+           </tr>
+           <tr>
+                   <td style="padding:10px;">
+                           <b>First Name:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["f_name"] . '</td>
+           </tr>
+
+           <tr>
+                   <td style="padding:10px;">
+                           <b>Last Name:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["l_name"] . '</td>
+           </tr>
+
+           
+           <tr>
+                   <td style="padding:10px;">
+                           <b>E-mail:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["email"] . '</td>
+           </tr>
+           <tr>
+                   <td style="padding:10px;">
+                           <b>Contact:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["phone"] . '</td>
+           </tr>
+
+
+           <tr>
+                   <td style="padding:10px;">
+                           <b>Country:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["country"] . '</td>
+           </tr>
+           <tr>
+                   <td style="padding:10px;">
+                           <b>Company:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["company"] . '</td>
+           </tr>
+
+           <tr>
+                   <td style="padding:10px;">
+                           <b>Designation:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["designation"] . '</td>
+           </tr>
+           
+           <tr>
+                   <td style="padding:10px;">
+                           <b>Message:</b>
+                   </td>
+                   <td style="padding:10px;">' . $_POST["message"] . '</td>
+           </tr>
+   </table>';
+
+
+   // End building message
+
+
+
+
+
+   try {
+       //Server settings
+       $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+       $mail->isSMTP();                                            // Send using SMTP
+       $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+       $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+       $mail->Username   = getenv("USERNAME");                     // SMTP username
+       $mail->Password   = getenv("Password");                               // SMTP password
+       $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+       $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+       //Recipients
+       $mail->setFrom('admin@limechat.ai', 'Limechat AI');
+       $mail->addAddress('admin@limechat.ai', 'Limechat AI');     // Add a recipient
+       $mail->addReplyTo('admin@limechat.ai', 'Limechat AI');
+
+
+       // Content
+       $mail->isHTML(true);                                  // Set email format to HTML
+       $mail->Subject = 'New Get Demo Form Submit';
+       $mail->Body    = $MESSAGE_BODY;
+       $mail->AltBody = 'HTML MESSAGE IS NOT SUPPORT PLEASE CHECK';
+
+       $mail->send();
+       echo 'Message has been sent';
+   } catch (Exception $e) {
+       echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+   }
+
+   echo '<script type="text/javascript"> window.onload = ()=>{ $(".thank-button").click(); } </script>';
+
+
+}
+?>
+   
+</div>
+
+
+
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary thank-button" data-toggle="modal" data-target="#exampleModal" style="display: none;">
+  Launch demo modal
+</button>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Thank you</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Thank for contacting is we will reach out to you soon.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>   
+
+
+<div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
